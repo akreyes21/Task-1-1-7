a2236d63950063707e9827305b22a69e4f80e184
akreyes21 <164187372+akreyes21@users.noreply.github.com>
2024-04-11 02:56:54 +0000

    No comment

--- Main.java
+++ Main.java
@@ -1,1 +1,26 @@
+public class Main{
+   public static void main(String[] args) {
+    def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+if y != 0:
+return x / y
+ else:
+return "Cannot divide by zero"
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
+}
 
